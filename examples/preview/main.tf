13b729f6c9e1969c9801061358847ceb4b22cefd
Wictor Zhao <wictor_x@hotmail.com>
2025-07-15 14:37:30 +0200

    Update all examples and include preview url example

--- examples/preview/main.tf
+++ examples/preview/main.tf
@@ -15,12 +15,27 @@ module "s3_static_files" {
   service_name = local.service_name
 }
 
+module "preview_url_mapper" {
+  count  = var.environment == "test" ? 1 : 0
+  source = "github.com/nsbno/terraform-aws-preview-url?ref=x.y.z"
+
+  providers = {
+    aws.us_east_1 = aws.us_east_1
+  }
+
+  service_name = local.service_name
+}
+
 module "ssr" {
   source = "../../terraform-aws-ssr-site"
 
   providers = {
     aws.certificate_provider = aws.us_east_1
   }
+
+  # Use preview environments only in test environment
+  enable_wildcard_domain        = var.environment == "test" ? true : false
+  preview_url_mapper_lambda_arn = var.environment == "test" ? module.preview_url_mapper[0].lambda_function_qualifier_arn : ""
 
   service_name    = local.service_name
   domain_name     = local.domain_name
@@ -30,3 +45,4 @@ module "ssr" {
 
   s3_website_endpoint = module.s3_static_files.website_endpoint
 }
+
---
+++ examples/preview/variables.tf
@@ -0,0 +1,4 @@
+variable "environment" {
+  description = "The environment to use for preview deployment"
+  type        = string
+}
