268cb2ddeb8dfaf8a2ac9752c12fe6cb25ece76c
Wictor Zhao <wictor_x@hotmail.com>
2025-07-15 14:26:10 +0200

    Add module for s3 static files

---
+++ modules/s3_static_files/main.tf
@@ -0,0 +1,52 @@
+data "aws_caller_identity" "this" {}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "${data.aws_caller_identity.this.account_id}-${var.application_name}-static-files"
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.this.id}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_website_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+locals {
+  ssm_parameters = {
+    static_files_bucket_name = aws_s3_bucket.this.id
+  }
+}
+
+resource "aws_ssm_parameter" "ssm_parameters" {
+  for_each = local.ssm_parameters
+
+  name  = "/__deployment__/applications/${var.application_name}/${each.key}"
+  type  = "String"
+  value = each.value
+}
