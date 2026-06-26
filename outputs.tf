41888617aac3e6968f73974b5450850a21c94eb5
Fredrik Førde Lindhagen <fredrik@lindhagen.io>
2026-06-26 11:52:16 +0200

    Add support for VPC origin

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "cloudfront_distribution_domain_name" {
   description = "The domain name corresponding to the distribution."
   value       = try(aws_cloudfront_distribution.this.domain_name, "")
 }
+
+output "certificate_validation_dns_records" {
+  value = [for record in aws_route53_record.cert_validation : record.fqdn]
+  description = "Route53 records for the doing DNS certificate validation of variable `domain_name`"
+}
